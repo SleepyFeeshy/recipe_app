04c4ab0f13d83532f3865e14bb7e577d25c59c7d
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-06 09:20:41 +0800

    feat: define FullRecipeIngredient domain model

--- lib/domain/models/recipe_ingredient/recipe_ingredient.dart
+++ lib/domain/models/recipe_ingredient/recipe_ingredient.dart
@@ -1,9 +1,18 @@
 class RecipeIngredient {
-  RecipeIngredient({required this.id, required this.ingredientId, required this.recipeId, required this.ingredientName});
+  RecipeIngredient({required this.id,  required this.recipeId, required this.ingredientId, required this.ingredientName });
 
   final String id;
-  final String ingredientId;
   final String recipeId;
+  final String ingredientId;
+  final String ingredientName;
+}
 
+class FullRecipeIngredient {
+  FullRecipeIngredient({required this.id,  required this.recipeId, required this.recipeName, required this.ingredientId, required this.ingredientName });
+
+  final String id;
+  final String recipeId;
+  final String recipeName;
+  final String ingredientId;
   final String ingredientName;
 }
