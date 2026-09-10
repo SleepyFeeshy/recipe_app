dfd136a70fc5794e44314e0a23ac2acee9452535
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-10 13:13:27 +0800

    Create shopping_list.dart

---
+++ lib/data/model/shopping_list.dart
@@ -0,0 +1,7 @@
+class ShoppingListEntity {
+  ShoppingListEntity({required this.id, required this.name, required this.createdAt});
+
+  final String id;  
+  final String name;
+  final String createdAt;
+}
